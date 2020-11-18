184f1ef0f5b7046f3f8139fd4722695861f61c61
Ethan B <eebmagic@gmail.com>
2020-11-17 20:20:38 -0500

    better comments and remove unused split_channels()

--- script.py
+++ script.py
@@ -3,42 +3,12 @@ import numpy as np
 import array
 import time
 
-def split_channels(s):
-    arr = s.get_array_of_samples()
-    if s.channels <= 1:
-        return arr
-    elif s.channels == 2:
-        left = []
-        right = []
-        for ind in range(len(arr)):
-            if ind % 2 == 0:
-                left.append(arr[ind])
-            else:
-                right.append(arr[ind])
-
-        return (left, right)
-
 
 def amp(data, increase):
-    # new = [int(point * increase) for point in data]
-    # return new
     return data * increase
 
 
 def cutoff_max(data, cutoff):
-    # cutoff = abs(cutoff)
-    # new = []
-    # for point in data:
-    #     if abs(point) > cutoff:
-    #         if point > 0:
-    #             new.append(cutoff)
-    #         else:
-    #             new.append(-cutoff)
-    #     else:
-    #         new.append(point)
-
-    # return new
-
     return np.clip(data, -cutoff, cutoff)
 
 
@@ -53,21 +23,30 @@ def abreviate(data, rate):
 
 start = time.time()
 
+# Load file
 input_file = "sources/greater_than_four_chords.mp3"
 sound = AudioSegment.from_file(input_file)
 print(f"{time.time() - start}\t: load sound")
 
+# Load samples
 both = sound.get_array_of_samples()
 both = np.array(both)
 print(f"{time.time() - start}\t: load samples")
+
+# Amplify samples
 amped = amp(both, 1.1)
 print(f"{time.time() - start}\t: amplify samples")
+
+# Cutoff samples
 cut = cutoff_max(amped, int(max(both) * 0.6))
 cut = cut.astype('int64')
 print(f"{time.time() - start}\t: cutoff samples")
+
+# Convert numpy.array to array.array
 cut = array.array(sound.array_type, cut)
 print(f"{time.time() - start}\t: put samples in array")
 
+# Build new sound object
 new = AudioSegment(
     data=cut,
     frame_rate = sound.frame_rate,
@@ -76,11 +55,9 @@ new = AudioSegment(
 )
 print(f"{time.time() - start}\t: build new sound")
 
-# print("Original sample size: " + '{:,}'.format(len(both)))
-# print("Output sample size: " + '{:,}'.format(len(new.get_array_of_samples())))
-
+# Export OUTPUT_ file
 output_file = "OUTPUT_" + input_file.split('/')[-1].split('.')[0] + ".wav"
-
 new.export(output_file, format="wav")
 print(f"{time.time() - start}\t: export file")
+
 print(f"\nFINISHED EXPORT: {output_file}")
